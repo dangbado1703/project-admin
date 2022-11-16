f79e4fb57b8449f8f3527645cbf28b04471d4aaa
Vinh17112k <tranquangvinh17112k@gmail.com>
2022-11-16 07:44:18 +0700

    danh muc san pham

---
+++ src/pages/DanhMuc/DanhMucPage.tsx
@@ -0,0 +1,57 @@
+import { Key } from "antd/lib/table/interface";
+import React, { useEffect, useState } from "react";
+import FormSearch from "../../component/DanhMuc/FormSearch";
+import FormTable from "../../component/DanhMuc/FormTable";
+import { IFormSearchDanhMuc } from "../../model/DanhMuc.model";
+import { useAppDispatch } from "../../store/hooks";
+import {
+  getName,
+  getCode,
+  getCreatedBy,
+  getDanhMuc,
+  getParent,
+} from "./danhmuc.reducer";
+
+const DanhMucPage = () => {
+  const [valueSearch, setValueSearch] = useState<IFormSearchDanhMuc>({
+    name: "",
+    code: "",
+    createdBy: "",
+  });
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(10);
+  const [selectedRowKeys, setSelectedRowKeys] = useState<Key[]>([]);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getDanhMuc({ ...valueSearch, page, size }));
+  }, [dispatch, valueSearch, page, size]);
+  useEffect(() => {
+    Promise.any([         
+      dispatch(getCode()),
+      dispatch(getName()),
+      dispatch(getCreatedBy()),
+      dispatch(getParent()),
+    ]);
+  }, [dispatch]);
+  return (
+    <div>
+      <FormSearch
+        selectedRowKeys={selectedRowKeys}
+        setValueSearch={setValueSearch}
+        setSelectedRowKeys={setSelectedRowKeys}
+        valueSearch={valueSearch}
+      />
+      <FormTable
+        page={page}
+        size={size}
+        valueSearch={valueSearch}
+        setPage={setPage}
+        setSize={setSize}
+        setSelectedRowKeys={setSelectedRowKeys}
+        selectedRowKeys={selectedRowKeys}
+      />
+    </div>
+  );
+};
+
+export default DanhMucPage;
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -4,6 +4,7 @@ import registerReducer from "../pages/Register/Register.reducer";
 import clientReducer from "../pages/Client/client.reducer";
 import productReducer from "../pages/Product/product.reducer";
 import staffReducer from "../pages/Staff/staff.reducer";
+import danhMucReducer from "../pages/DanhMuc/danhmuc.reducer";
 
 const rootReducer = combineReducers({
   loginReducer,
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
   clientReducer,
   productReducer,
   staffReducer,
+  danhMucReducer
 });
 
 export default rootReducer;
